e2eb7fc7a21727678cbb1861a162aa92363cd015
Tannert <223003068@logon.ds.ge.com>
2018-01-17 15:13:01 -0500

    Complete

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -17,45 +17,57 @@ function loadData() {
 
     $greeting.text('So, you want to live at ' + address + '?');
 
-
     // load streetview
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
 
+
+
     // load nytimes
-    
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-	url += '?' + $.param({
-		'api-key': "ae1411f93d694f659eb59d1115538adb",
-		'q': $('#city').val()
-	});
-	console.log(url);
-	$.getJSON(url, function (data) {
-		$nytHeaderElem.text("New York Times articles about " + $('#city').val());
-		articles = data.response.docs;
-		for (var i = 0; i < articles.length; i++) {
-			var article = articles[i];
-			console.log(article);
-			$nytElem.append('<li class="article">');
-			$nytElem.append('<a href="' + article.web_url + '">' + article.headline.main + '</a>');
-			$nytElem.append('<p>' + article.snippet + '</p>');
-			$nytElem.append('</li>');
-		}
-	}).error(function(e) {
-		$nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
-	});
-	
-	
-	//articles = data.response.docs;
-	//console.log(articles.length);
-	//console.log(articles.length[0]);
-	//for (i=0; i < data.response.docs.length; i++) {
-	//	$nytElem.append('<li class="article">');
-	//	$nytElem.append('<a href="' + a.web_url + '">' + a.headline.main + '</a>');
-	//	$nytElem.append('<p>' + a.multimedia.snippet + '</p>');
-	//	$nytElem.append('</li>');
-	//}
+    // obviously, replace all the "X"s with your own API key
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=ae1411f93d694f659eb59d1115538adb';
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+
+
+    // load wikipedia data
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function( response ) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
 
     return false;
 };
