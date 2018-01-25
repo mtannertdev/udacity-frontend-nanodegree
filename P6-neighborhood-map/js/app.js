9f74ffa58098d5f35581754a409edb982bcbb090
Mark Tannert <mark@tannert.net>
2018-01-25 00:15:27 -0500

    Added Search, not working

--- P6-neighborhood-map/js/app.js
+++ P6-neighborhood-map/js/app.js
@@ -9,7 +9,7 @@ var model = [
         lat: 39.328855,
         lng: -84.426399,
         selected: false,
-        visible: true,
+        shown: true,
 		yelp_location: "jags-steak-and-seafood-and-piano-bar-west-chester-5"
     },
     {
@@ -17,28 +17,32 @@ var model = [
         lat: 39.187166,
         lng: -84.364779,
         selected: false,
-        visible: true,
+        shown: true,
+		yelp_location: "ferraris-little-italy-and-bakery-madeira-2"
     },
     {
         name: "Tong's Thai",
         lat: 39.182351,
         lng: -84.274580,
         selected: false,
-        visible: true,
+        shown: true,
+		yelp_location: "tongs-thai-milford"
     },
     {
         name: "Andy's Mediterranean Grille",
         lat: 39.120899,
         lng: -84.493498,
         selected: false,
-        visible: true,
+        shown: true,
+		yelp_location: "andys-mediterranean-grill-cincinnati"
     },
     {
         name: "Blind Pig",
         lat:  39.098807,
         lng:  -84.513403,
         selected: false,
-        visible: true,
+        shown: true,
+		yelp_location: "blind-pig-cincinnati"
     }
 ];
 
@@ -54,16 +58,18 @@ var viewModel = function() {
 			position: {lat: location.lat, lng: location.lng},
 			map: map,
 			name: location.name,
+			//yelp_data: getYelpInfo(location.yelp_location),
 			yelp_location: location.yelp_location,
-			//visible: ko.observable(location.visible),
+			shown: ko.observable(location.shown),
 			selected: ko.observable(location.selected),
 			animation: google.maps.Animation.DROP
 		}));
 	});
 
+
 	// Add listeners to each locationMarker
-	for (var i=0; i < self.locationMarkerList; i++) {
-		marker.addListener('click', function() {
+	for (var i=0; i < self.locationMarkerList.length; i++) {
+		self.locationMarkerList[i].addListener('click', function() {
 			self.selectLocation(this);
 		});
 	}
@@ -79,44 +85,78 @@ var viewModel = function() {
 		self.selectedLocationMarker.setAnimation(google.maps.Animation.NONE);
 		locationMarker.setAnimation(google.maps.Animation.BOUNCE);
 
+		var infoWindowContent = "<h5>" + locationMarker.name + "</h5>";
+		if (locationMarker.yelp_data != null) {
+			infoWindowContent += "<div>Yelp rated "+ locationMarker.yelp_data.rating + "/5 with " + locationMarker.yelp_data.review_count + " reviews</div>";
+        } else {
+			infoWindowContent += "<div>Yelp data not available yet</div>";
+		}
+		self.locationMarkerInfoWindow.setContent(infoWindowContent);		
         self.locationMarkerInfoWindow.open(map, locationMarker);
-		
-	//	var yelp_json = getYelpInfo(locationMarker.yelp_location);
-	//	self.locationMarkerInfoWindow.setContent('<h5>' + locationMarker.name + '</h5>' +
-	//		'<div>Yelp data still loading, please try again in a moment</div>');
-        /*
-		formattedLikes = function() {
-        	if (self.currentMapItem.likes === "" || self.currentMapItem.likes === undefined) {
-        		return "No likes to display";
-        	} else {
-        		return "Location has " + self.currentMapItem.likes;
-        	}
-        };
-
-        formattedRating = function() {
-        	if (self.currentMapItem.rating === "" || self.currentMapItem.rating === undefined) {
-        		return "No rating to display";
-        	} else {
-        		return "Location is rated " + self.currentMapItem.rating;
-        	}
-        };
-
-        var formattedInfoWindow = "<h5>" + self.currentMapItem.name + "</h5>" + "<div>" + formattedLikes() + "</div>" + "<div>" + formattedRating() + "</div>";
-
-		infowindow.setContent(formattedInfoWindow);
-
-        infowindow.open(map, locationMarker);
-		*/
-
 		
 		self.selectedLocationMarker = locationMarker;
 	};
+	
+	// create a search observable for search text
+	self.searchText = ko.observable();
+
+	// calls every keydown from input box
+	self.updateLocations = function() {
+		self.locationMarkerInfoWindow.close();
+
+		var search = self.searchText();
+
+		//search the list
+		if (search.length === 0) {
+			for (var i=0; i < self.locationMarkerList.length; i++) {
+				self.locationMarkerList[i].setMap(map);
+				self.locationMarkerList[i].shown = true;
+			}
+		} else {
+			for (var i=0; i < self.locationMarkerList.length; i++) {
+				if (self.locationMarkerList[i].name.toLowerCase().indexOf(search.toLowerCase()) > -1) {
+					self.locationMarkerList[i].setMap(map);
+					self.locationMarkerList[i].shown = true;
+				} else {
+					self.locationMarkerList[i].setMap(null);
+					self.locationMarkerList[i].shown = false;
+				}
+			}
+		}
+	};
+	
+	
+	self.addYelpData = function(yelp_location) {
+		/* Referenced this for example of a CORS workaround since Yelp does not support jsonp */
+		/* https://forum.freecodecamp.org/t/authorization-http-header-for-yelp-fusion-api-access-token/140974/4 */
+		const access_token = "qFQ8Y7KRTh-wM9zV8teJ30mtqemAKGskq4EgH58_PaWOxWJxPD3IiEefjSAksafIFGhTbEmH0ddQ4cHRk1oWq09FSmCckFQwIpm-Rc4VAnED5sWZYlDxvKsojTlpWnYx";
+		
+		let myHeaders = new Headers();
+		myHeaders.append("Authorization", "Bearer " + access_token);
+
+		fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/"+yelp_location, {
+			headers: myHeaders 
+		}).then((res) => {
+			return res.json();
+		}).then((json) => {
+			for (var i=0; i < self.locationMarkerList.length; i++) {
+				if (self.locationMarkerList[i].yelp_location == yelp_location) {
+					self.locationMarkerList[i].yelp_data = json;
+				}
+			}
+		});
+	};
+
+	// Add Yelp data to each locationMarker
+	for (var i=0; i < self.locationMarkerList.length; i++) {
+		self.addYelpData(self.locationMarkerList[i].yelp_location)
+	}
 }
 
 // this functon runs when the Google Maps API loads
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 39.1719717, lng: -84.5306452},
+		center: {lat: 39.2119717, lng: -84.4306452},
 		zoom: 11,
 		mapTypeControl: false,
 		fullscreenControl: false
@@ -129,22 +169,4 @@ function initMap() {
 function googleError() {
 	alert("Failed to load Google Maps API");
 }
-
-function getYelpInfo(yelp_location) {
-	/* Referenced this for example of a CORS workaround since Yelp does not support jsonp */
-	/* https://forum.freecodecamp.org/t/authorization-http-header-for-yelp-fusion-api-access-token/140974/4 */
-	const access_token = "qFQ8Y7KRTh-wM9zV8teJ30mtqemAKGskq4EgH58_PaWOxWJxPD3IiEefjSAksafIFGhTbEmH0ddQ4cHRk1oWq09FSmCckFQwIpm-Rc4VAnED5sWZYlDxvKsojTlpWnYx";
-	
-	let myHeaders = new Headers();
-	myHeaders.append("Authorization", "Bearer " + access_token);
-
-	fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/"+yelp_location, {
-		headers: myHeaders 
-	}).then((res) => {
-		return res.json();
-	}).then((json) => {
-		console.log(json);
-		return json;
-	});
-};
 
