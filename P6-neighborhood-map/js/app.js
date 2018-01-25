4d83c4ba2919a64adbb65841945e4be46866fd23
Mark Tannert <mark@tannert.net>
2018-01-24 22:17:30 -0500

    Added Yelp API

--- P6-neighborhood-map/js/app.js
+++ P6-neighborhood-map/js/app.js
@@ -10,6 +10,7 @@ var model = [
         lng: -84.426399,
         selected: false,
         visible: true,
+		yelp_location: "jags-steak-and-seafood-and-piano-bar-west-chester-5"
     },
     {
         name: "Ferrari's Little Italy and Bakery",
@@ -41,28 +42,75 @@ var model = [
     }
 ];
 
-/* ***** Biew Model ***** */
+/* ***** View Model ***** */
 
 var viewModel = function() {
 	var self = this;
 
-	// Generate the location list
-	self.locationList = [];
+	// Generate the locationMarker list from the model
+	self.locationMarkerList = [];
 	model.forEach(function(location){
-		self.locationList.push(new google.maps.Marker({
+		self.locationMarkerList.push(new google.maps.Marker({
 			position: {lat: location.lat, lng: location.lng},
 			map: map,
 			name: location.name,
+			yelp_location: location.yelp_location,
 			//visible: ko.observable(location.visible),
 			selected: ko.observable(location.selected),
 			animation: google.maps.Animation.DROP
 		}));
 	});
-	
-	// The currently selected location
-	self.currentLocation = self.locationList[0];
-	
-	
+
+	// Add listeners to each locationMarker
+	for (var i=0; i < self.locationMarkerList; i++) {
+		marker.addListener('click', function() {
+			self.selectLocation(this);
+		});
+	}
+
+	self.locationMarkerInfoWindow = new google.maps.InfoWindow();
+	self.selectedLocationMarker = self.locationMarkerList[0];
+
+	// Define handler for when a location is selected
+	self.selectLocation = function(locationMarker) {
+		if (location.name == self.selectedLocationMarker.name) {
+			return;
+		}
+		self.selectedLocationMarker.setAnimation(google.maps.Animation.NONE);
+		locationMarker.setAnimation(google.maps.Animation.BOUNCE);
+
+        self.locationMarkerInfoWindow.open(map, locationMarker);
+		
+	//	var yelp_json = getYelpInfo(locationMarker.yelp_location);
+	//	self.locationMarkerInfoWindow.setContent('<h5>' + locationMarker.name + '</h5>' +
+	//		'<div>Yelp data still loading, please try again in a moment</div>');
+        /*
+		formattedLikes = function() {
+        	if (self.currentMapItem.likes === "" || self.currentMapItem.likes === undefined) {
+        		return "No likes to display";
+        	} else {
+        		return "Location has " + self.currentMapItem.likes;
+        	}
+        };
+
+        formattedRating = function() {
+        	if (self.currentMapItem.rating === "" || self.currentMapItem.rating === undefined) {
+        		return "No rating to display";
+        	} else {
+        		return "Location is rated " + self.currentMapItem.rating;
+        	}
+        };
+
+        var formattedInfoWindow = "<h5>" + self.currentMapItem.name + "</h5>" + "<div>" + formattedLikes() + "</div>" + "<div>" + formattedRating() + "</div>";
+
+		infowindow.setContent(formattedInfoWindow);
+
+        infowindow.open(map, locationMarker);
+		*/
+
+		
+		self.selectedLocationMarker = locationMarker;
+	};
 }
 
 // this functon runs when the Google Maps API loads
@@ -81,4 +129,22 @@ function initMap() {
 function googleError() {
 	alert("Failed to load Google Maps API");
 }
+
+function getYelpInfo(yelp_location) {
+	/* Referenced this for example of a CORS workaround since Yelp does not support jsonp */
+	/* https://forum.freecodecamp.org/t/authorization-http-header-for-yelp-fusion-api-access-token/140974/4 */
+	const access_token = "qFQ8Y7KRTh-wM9zV8teJ30mtqemAKGskq4EgH58_PaWOxWJxPD3IiEefjSAksafIFGhTbEmH0ddQ4cHRk1oWq09FSmCckFQwIpm-Rc4VAnED5sWZYlDxvKsojTlpWnYx";
+	
+	let myHeaders = new Headers();
+	myHeaders.append("Authorization", "Bearer " + access_token);
+
+	fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/"+yelp_location, {
+		headers: myHeaders 
+	}).then((res) => {
+		return res.json();
+	}).then((json) => {
+		console.log(json);
+		return json;
+	});
+};
 
